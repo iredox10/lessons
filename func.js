a90eb849cb3a03340e537bfc17de16c991971680
hassan <idreesadam200@gmail.com>
2024-01-20 15:29:29 +0100

    first lesson

---
+++ func.js
@@ -0,0 +1,5 @@
+//! function is first-class citizen 
+//! in js.
+
+//! function returned value can be assign to a variable
+//! function can be pass to another function as a parameter
---
+++ script.js
@@ -0,0 +1,22 @@
+const arr = [8,9,5,3,6]
+
+
+const arr1 = new Array(6,7,7,8,) || []
+
+const obj = {} || new Object()
+
+const obj1 = {
+    name: 'ali',
+    age: 18,
+    bio: function (){console.log('obj1 function')}
+}
+
+// console.log(arr.reverse())
+// console.log(obj1.bio())
+
+function name(){
+    
+}
+
+let u = ''
+console.log(u)
